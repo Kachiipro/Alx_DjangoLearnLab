2b656dc62c3614b62ed498b8e01cecdb89b2a219
Dikachi J <dikachimichael253@gmail.com>
2024-11-02 05:43:49 +0100

    created admin str

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,3 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_year =models.IntegerField(default=0000)
+
+
+    def __str__(self):
+        return f"{self.title} {self.author} {self.published_year}"
