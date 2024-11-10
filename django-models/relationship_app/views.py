8ff60b8a8cde92bde6ab2076b8d47ae7edbbfec1
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 20:18:03 +0100

    completed user access control

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 from .models import UserProfile
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -27,13 +28,37 @@ class register(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
-    
-@user_passes_test
-def is_admin(self):
-        return self.role == 'Admin'
-@user_passes_test  
-def is_librarian(self):
-        return self.role == 'Librarian'
-@user_passes_test
-def is_member(self):
-        return self.role == 'Member'
+
+def is_admin(user):
+    try:
+        return user.userprofile.role == 'Admin'
+    except UserProfile.DoesNotExist:
+        return False
+
+@user_passes_test(is_admin)  # Restrict access to Admin users only
+def admin_dashboard(request):
+    return render(request, 'relationship_app/admin_dashboard.html')
+
+
+
+
+def is_librarian(user):
+    try:
+        return user.userprofile.role == 'Librarian'
+    except UserProfile.DoesNotExist:
+        return False
+
+@user_passes_test(is_librarian)  
+def librarian_dashboard(request):
+    return render(request, 'relationship_app/librarian_dashboard.html')
+
+
+def is_member(user):
+    try:
+        return user.userprofile.role == 'Member'
+    except UserProfile.DoesNotExist:
+        return False
+
+@user_passes_test(is_member) 
+def member_dashboard(request):
+    return render(request, 'relationship_app/member_dashboard.html')
