5e31fd2d4c48244c380ce205dbb3a27f257365d0
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 14:46:11 +0100

    detail view url config

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .views import list_books,LibraryDetailView
 urlpatterns= [
-    path('', views.book_list, name = 'book_list')
+    path('', views= list_books, name = 'book_list'),
+    path('', views= LibraryDetailView, name = 'book_list')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,12 +5,12 @@ from .models import Library
 
 # Create your views here.
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
 
-class library_list(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
