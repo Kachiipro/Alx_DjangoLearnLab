b185fbd5268c9b53ec1a3e5e731fc329aee591ce
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 17:24:08 +0100

    admin func

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from .models import UserProfile
 
 # Create your views here.
 
@@ -26,3 +27,13 @@ class register(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+def is_admin(user):
+    try:
+        return user.userprofile.role == 'Admin'
+    except UserProfile.DoesNotExist:
+        return False
+
+@user_passes_test(is_admin)  # Restrict access to Admin users only
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
