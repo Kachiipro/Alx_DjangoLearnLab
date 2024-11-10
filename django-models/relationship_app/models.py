3da015e50587e2b32c8c2b8c4750b4c46886fa15
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 16:27:31 +0100

    implementing custom permision

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -24,3 +24,24 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+        ]
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='user')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}" 
+    
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
