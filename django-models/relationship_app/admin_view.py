eb0584a3488b02d2d0963fde00bae66d5ce408a6
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 17:19:59 +0100

    admin cotrol access

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 from .models import UserProfile
 
+# Role-checking function for admin role
+def is_admin(user):
+    try:
+        return user.userprofile.role == 'Admin'
+    except UserProfile.DoesNotExist:
+        return False
+
+@user_passes_test(is_admin)  # Restrict access to Admin users only
 def admin_dashboard(request):
-    user_profile = UserProfile.objects.get(user=request.user)
-    if user_profile.is_admin():
-        return render(request, 'admin_dashboard.html')
-    return HttpResponseForbidden("You are not authorized to view this page.")
+    return render(request, 'admin_dashboard.html')
