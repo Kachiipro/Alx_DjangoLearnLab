0f533ed1f77e7f56de0b2bba2fdf44b5fb8e0cde
Dikachi J <dikachimichael253@gmail.com>
2024-11-10 13:49:23 +0100

    template for listing books

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'books/list_books.html', context)
+    
