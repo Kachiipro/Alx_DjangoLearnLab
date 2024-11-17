00fb20ebdf5d83c2a6350abaa3655f68784145f1
Dikachi J <dikachimichael253@gmail.com>
2024-11-17 20:47:23 +0100

    forms

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,13 +1,8 @@
 from django import forms
 
-class UserForm(forms.Form):
+class ExampleForm(forms.Form):
     username = forms.CharField(max_length=100)
     email = forms.EmailField()
 
-# In your view, use this form to handle user input
-def user_view(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            # Process the data
-            pass
+
+
