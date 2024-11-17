292ea9d139e98830d0e5c0b223fe089898b656ab
Dikachi J <dikachimichael253@gmail.com>
2024-11-17 16:04:44 +0100

    created custom user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} {self.author} {self.published_year}"
     
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
