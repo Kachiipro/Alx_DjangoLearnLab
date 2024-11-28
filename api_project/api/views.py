87c9177bf434e569eee17ef21b50a867abf486de
Dikachi J <dikachimichael253@gmail.com>
2024-11-28 02:34:35 +0100

    permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,12 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -11,3 +17,18 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
