bc5f4be31f198669928dd6a1588b6f12870815b8
Dikachi J <dikachimichael253@gmail.com>
2024-12-01 13:46:03 +0100

    created custom views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CustomBookCreateView,CustomBookDetailView,CustomBookListView,CustomBookDeleteView,CustomBookUpdateView
 
 urlpatterns = [
-    path('api/', CustomBookListView.as_view(), name='book-list'),
-    path('api/<int:pk>/', CustomBookDetailView.as_view(), name='book-detail'),
-    path('api/create/', CustomBookCreateView.as_view(), name='book-create'),
-    path('api/<int:pk>/update/', CustomBookUpdateView.as_view(), name='book-update'),
-    path('api/<int:pk>/delete/', CustomBookDeleteView.as_view(), name='book-delete'),
+    path('books/', CustomBookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', CustomBookDetailView.as_view(), name='book-detail'),
+    path('books/create/', CustomBookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', CustomBookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', CustomBookDeleteView.as_view(), name='book-delete'),
 ]
