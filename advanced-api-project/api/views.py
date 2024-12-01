00d1470cea7359398632a9fa91eeb19bd4648eb6
Dikachi J <dikachimichael253@gmail.com>
2024-12-01 14:01:04 +0100

    created custom views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book #replace with your working model
 from .serializers import BookSerializer
-from rest_framework.permission import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
  
 
 # Create your views here.
