d51bfbccdb78a54ae5fe2afdfdf4a919e26c3170
Dikachi J <dikachimichael253@gmail.com>
2024-12-01 13:20:55 +0100

    created custom views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book #replace with your working model
+from .serializers import BookSerializer
 
 # Create your views here.
+
+
+
+
+class CustomBookCreateView(generics.CreateAPIView):
+# can be any name, ensure to align with your project as this is sample exampls 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookDetailView(generics.DetailAPIView):
+
