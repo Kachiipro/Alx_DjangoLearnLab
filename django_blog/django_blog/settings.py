9573b0871d66aabaf2c32433ecef652b4bd4079c
Dikachi J <dikachimichael253@gmail.com>
2024-12-06 03:39:05 +0100

    created a blog and a custom user

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
-    'users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.User'
