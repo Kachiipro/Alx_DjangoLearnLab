e45376945f056014fbabc0fc3cec90221967f700
Dikachi J <dikachimichael253@gmail.com>
2024-12-16 09:08:13 +0100

    added serializers and urls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -21,11 +21,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # Create the user
         user = get_user_model().objects.create_user(
-            username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None),
         )
         # Automatically create a token for the new user
         Token.objects.create(user=user)
